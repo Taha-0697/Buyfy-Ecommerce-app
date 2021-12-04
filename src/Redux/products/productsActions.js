e2038c5210273d0196138e7fd3aebea0bf874b26
Mutahhir Khan <mutahhir199@gmail.com>
2021-12-04 15:22:57 +0500

    category page

--- src/Redux/products/productsActions.js
+++ src/Redux/products/productsActions.js
@@ -1,5 +1,6 @@
 import { firestore, serverTimestamp, storage } from "../../Firebase/Firebase";
 import { v4 as uuid } from "uuid";
+import { SET_PRODUCTS } from './productsConstants';
 
 
 // ADMIN SIDE STUFF 
@@ -60,6 +61,64 @@ export const uploadProduct = (productObj) => async () => {
 
 
 // Product SIDE STUFF
+
+const catagorizedProducts = (productsArr) => {
+  console.log("%cCategorized Product Function: \n","color:green;font-weight:bolder" ,productsArr)
+
+  // 1- initialize catagories Array
+  
+  // Structure of catagories Array
+  // const catagories = [
+  //   {
+  //     category:"men",
+  //     product: [{},{},{}]
+  //   },
+  //   {
+  //     category:"glass",
+  //     product: [{},{},{}]
+  //   },
+  //   {
+  //     category:"Hoodie",
+  //     product: [{},{},{}]
+  //   }
+  // ]
+  
+  const categoriesArr = [];
+  // 2- loop through products
+  console.log("%cProduct Category Exist or Not:","color:green;font-weight:bolder")
+  for(const product of productsArr){
+      const categoryName = product.category;
+     // console.log(categoryName);
+
+    // 3- Loop through categoriesArr / check if category exist in categoriesArr
+    const isCategoryExist= categoriesArr.some((categoryObj) => categoryObj.category === categoryName)
+    console.log("%cIsCategoryExist:","color:green;font-weight:bolder",isCategoryExist)
+    
+    // If Category Exists
+    if(isCategoryExist){
+      // push product in products field in categoryObj 
+      categoriesArr= categoriesArr.map((categoryObj) => {
+        if(categoryObj.category === categoryName){
+          categoryObj.products.push(product)
+        }
+      })
+    }
+     // If Category Doesn't Exists
+    else{
+      // create new categoryObj
+      const newCategoryObj = {
+        category: product.category,
+        products: [product]
+      }
+      // push newCategoryObj in categoriesArr
+      categoriesArr.push(newCategoryObj) 
+    }
+  
+  }    
+  console.log("%cCategories:","color:green;font-weight:bolder",categoriesArr)
+}
+
+
 export const fetchProducts = () => async (dispatch) => {
 try {
   const query = await firestore.collection("products").get();
@@ -67,12 +126,16 @@ try {
   query.docs.forEach((doc) => {
   products.push(doc.data())
   })
-  console.log(products);
+console.log("%cProducts Array: \n", "font-weight:bolder;color:green", products);
+  catagorizedProducts(products);
 
-  dispatch({
-    type: "SET_PRODUCTS",
-    payload: products,
-  })
+
+  // dispatch({
+  //   type: SET_PRODUCTS,
+  //   payload: {
+  //     products, // Array
+  //   }, 
+  // })
 
 } catch (error) {
   console.log(error)
