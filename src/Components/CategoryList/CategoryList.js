4fabe782386344ea085bd02fb6f3772c796a0f82
Muhammad Owais <malikowaisjavaid@gmail.com>
2021-12-06 15:03:24 +0500

    updated

--- src/Components/CategoryList/CategoryList.js
+++ src/Components/CategoryList/CategoryList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import CategoryListItems from '../CategoryListItems/CategoryListItems'
 import { connect } from 'react-redux';
 import { fetchProducts } from '../../Redux/products/productsActions';
